820b8ace1b9999c181c5fa6798d33603ee839f6f
Soslyne Moreira <soslynemoreira@outlook.com>
2020-12-01 12:54:20 -0300

    Projeto teste pra prova!

---
+++ src/main/java/testandoMaven/TestandoMaven.java
@@ -0,0 +1,15 @@
+
+package testandoMaven;
+
+/**
+ *
+ * @author sosly
+ */
+public class TestandoMaven {
+    public static void main(String[] args) {
+        
+        System.out.println("Vai dar certo!");
+        
+    }
+    
+}
